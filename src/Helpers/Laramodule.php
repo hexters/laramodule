0aaed0cd7d49ab211b568942e6aa405822ae2311
Asep <hexters@gmail.com>
2022-04-26 15:16:28 +0700

    fix module path

--- src/Helpers/Laramodule.php
+++ src/Helpers/Laramodule.php
@@ -7,6 +7,7 @@ if (!function_exists('module_path')) {
     function module_path($name, $path = null)
     {
 
+        $name = ucwords($name);
         $folder = 'Modules/';
         $path = $path ? '/' . ltrim($path, '/') : null;
         return base_path("{$folder}{$name}{$path}");
